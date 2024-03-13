60887abd36dcfcaf204a9eeba9e0abc2c8194273
Hrithik Roy <hrithikroy1854@gmail.com>
2024-03-13 22:54:31 +0530

    Second Commit

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,9 +1,11 @@
 import DashboardLayout from "../../components/DashboardLayout";
 import PortfolioSection from "./components/PortfolioSection";
+import PriceSection from "./components/PriceSection";
 const Dashboard = () => {
   return (
     <DashboardLayout title="Dashboard">
       <PortfolioSection />
+      <PriceSection />
     </DashboardLayout>
   );
 };
--- src/pages/Dashboard/components/PriceSection.jsx
+++ src/pages/Dashboard/components/PriceSection.jsx
@@ -1,7 +1,34 @@
 import { CustomCard } from "../../../chakra/CustomCard";
-
+import { HStack, Stack, Text, Icon } from "@chakra-ui/react";
+import { BsArrowUpRight } from "react-icons/bs";
 const PriceSection = () => {
-  return <CustomCard>PriceSection</CustomCard>;
+  return (
+    <CustomCard>
+      {" "}
+      <Stack>
+        <HStack color="black.80">
+          <Text fontSize="14px">Wallet Balances</Text>
+        </HStack>
+        <HStack
+          spacing={2}
+          align={{ base: "flex-start", sm: "center" }}
+          flexDir={{ base: "column", sm: "row" }}
+        >
+          <HStack>
+            <Text textStyle="h2" fontWeight="medium">
+              22.39401000
+            </Text>
+            <HStack fontWeight="medium" color="green.500">
+              <Icon as={BsArrowUpRight} />
+              <Text textStyle="h2" fontWeight="medium" color="green.500">
+                22%
+              </Text>
+            </HStack>
+          </HStack>
+        </HStack>
+      </Stack>
+    </CustomCard>
+  );
 };
 
 export default PriceSection;
